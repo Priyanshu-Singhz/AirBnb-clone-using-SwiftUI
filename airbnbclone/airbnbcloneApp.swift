49dfd89135ab20fbda95b932a4ec63dcda00e892
“Nidhi <ps4151722@gmail.com>
2024-03-19 12:00:25 +0530

    Initial Commit

---
+++ airbnbclone/airbnbcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbcloneApp.swift
+//  airbnbclone
+//
+//  Created by Zignuts Technolab on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnbcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
